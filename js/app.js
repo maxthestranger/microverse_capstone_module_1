6827be53ada05509158f9f7a2f3a7bb8554f003d
maxthestranger <thestrangermax@gmail.com>
2022-03-07 16:54:23 +0300

    add dynamic instructor grid

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import instructorData from './data.js';
+
 // hamburger
 const btn = document.querySelector('.hamburger');
 const menu = document.querySelector('.navbar');
@@ -5,4 +7,35 @@ const menu = document.querySelector('.navbar');
 btn.addEventListener('click', () => {
   btn.classList.toggle('active');
   menu.classList.toggle('open');
+});
+
+// instructors
+const grid = document.querySelector('.grid');
+
+function createGridItem({
+  imgUrl, title, role, info,
+}) {
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid_item');
+
+  gridItem.innerHTML = `
+  <div class="in_img">
+    <img src="img/${imgUrl}" alt="${title}" />
+  </div>
+  <div>
+    <h3>${title}</h3>
+    <p class="italic text_primary styled_text">
+    ${role}
+    </p>
+    <p>
+    ${info}
+    </p>
+  </div>
+  `;
+
+  grid.appendChild(gridItem);
+}
+
+instructorData.forEach((instr) => {
+  createGridItem(instr);
 });
